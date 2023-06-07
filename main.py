b1bc067a9e435646dc2f4ee9f4b99b70fc8201fc
Hugo <gthugo@outlook.com>
2023-06-07 17:21:14 -0500

    Last two functions, get_director and get_actor

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ movies[['genres_id', 'actors_id', 'directors_id']] = movies[[
     'genres_id', 'actors_id', 'directors_id']].apply(lambda x: x.apply(eval))
 
 # Cargamos los datasets como diccionarios para un fácil acceso
-genres = pd.read_csv("genres.csv", index_col='id').to_dict(
-    orient='dict')['genre']
-actors = pd.read_csv("actors.csv", index_col='id').to_dict(
-    orient='dict')['actor']
-directors = pd.read_csv("directors.csv", index_col='id').to_dict(
-    orient='dict')['director']
+genres = pd.read_csv("genres.csv", index_col='genre').to_dict(
+    orient='dict')['id']
+actors = pd.read_csv("actors.csv", index_col='actor').to_dict(
+    orient='dict')['id']
+directors = pd.read_csv("directors.csv", index_col='director').to_dict(
+    orient='dict')['id']
 
 # Creamos el diccionario de meses para la ruta get months
 months = {
@@ -83,8 +83,8 @@ def films_per_day(day: str):
     return {"message": f"{amount} películas fueron estrenadas el día {day}."}
 
 
-# Función para obtener el score de una filmación por su título
-@app.get("/{movie}/{info}")
+# Función para obtener el score de una filmación por su título o sus votos por títulos
+@app.get("/movies/{movie}/{info}")
 def title_score(movie: str, info: str):
     # Lógica para obtener el título, año de estreno y score de la filmación
     movie = movie.lower()
@@ -120,3 +120,76 @@ def title_score(movie: str, info: str):
                 return {
                     "message": f"La película {title} fue estrenada en el año {year}. La misma cuenta con {vote_count} votos que dan un promedio de {vote_average} de 10."
                 }
+
+
+# Función para obtener el actor
+@app.get("/actors/{actor}")
+def get_actor(actor: str):
+    # Lógica para obtener el éxito del director y detalles de todas sus películas
+    actor = actor.lower()
+    r_actor = actor.title()
+    # Buscar el id del actor en el diccionario, si da un key error es porque no existe
+    try:
+        actor_id = actors[actor]
+    except KeyError:
+        return {"message": f"No se encontró el actor {r_actor} revise el dato"}
+
+    # Filtramos las películas en las que ha participado el actor
+    actor_films = movies[movies['actors_id'].apply(
+        lambda ids: actor_id in ids)]
+
+    if actor_films.empty:
+        return {"message": f"No se encontraron filmaciones para el actor {r_actor}"}
+
+    # Se calcula la cantidad de filmaciones y el retorno promedio se calcula solo de las filas en que el retorno es diferente a 0
+    films_return = actor_films[actor_films['return'] != 0]
+    avg_return = round(films_return['return'].mean(), 2)
+
+    return {"message": f"El actor {r_actor} ha participado de {len(actor_films)} cantidad de filmaciones, el mismo ha conseguido un retorno promedio de {avg_return} por filmación"}
+
+
+# Función para obtener el éxito de un director y detalles de sus películas
+@app.get("/directors/{director}")
+def get_director(director: str):
+    director = director.lower()
+    r_director = director.title()
+    # Buscamos el id del actor en el diccionario, si da un key error es porque no existe
+    try:
+        director_id = directors[director]
+    except KeyError:
+        return {"message": f"No se encontró el director {r_director} revise el dato"}
+    # Buscamos el director en el DataFrame de películas
+    director_films = movies[movies['actors_id'].apply(
+        lambda ids: director_id in ids)]
+
+    if director_films.empty:
+        return {"message": f"No se encontraron filmaciones para el director {r_director}"}
+
+    # Calculamos el éxito del director y los detalles de sus películas
+    peliculas = []
+    for _, row in director_films.iterrows():
+        titulo = row['title']
+        fecha_lanzamiento = row['release_date']
+        costo = row['budget']
+        ganancia = row['revenue']
+        retorno = round(row['return'], 2)
+        peliculas.append((titulo, fecha_lanzamiento, costo, ganancia, retorno))
+
+    # Sacamos el retorno en promedio de director
+    films_return = director_films[director_films['return'] != 0]
+    avg_return = round(films_return['return'].mean(), 2)
+
+    return {
+        "message": f'{r_director} ha dirigido {len(director_films)} películas',
+        "éxito": f'{avg_return} es el promedio de retorno de sus películas',
+        "peliculas": [
+            {
+                "titulo": titulo,
+                "fecha_lanzamiento": fecha_lanzamiento,
+                "costo": costo,
+                "ganancia": ganancia,
+                "retorno": retorno
+            }
+            for titulo, fecha_lanzamiento, costo, ganancia, retorno in peliculas
+        ]
+    }
