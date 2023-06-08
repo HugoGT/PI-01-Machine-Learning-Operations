0bfc02a7e34f27eee51c1fa79a733e702ed445d2
Hugo <gthugo@outlook.com>
2023-06-07 21:26:30 -0500

    Organized data

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from fastapi import FastAPI
 
 
 # Cargamos el dataset de películas como un dataframe de pandas
-movies = pd.read_csv("movies.csv", index_col='id')
+movies = pd.read_csv("data/movies.csv", index_col='id')
 
 # Convertimos estas columnas en listas para practicidad
 movies[['genres_id', 'actors_id', 'directors_id']] = movies[[
     'genres_id', 'actors_id', 'directors_id']].apply(lambda x: x.apply(eval))
 
 # Cargamos los datasets como diccionarios para un fácil acceso
-genres = pd.read_csv("genres.csv", index_col='genre').to_dict(
+genres = pd.read_csv("data/genres.csv", index_col='genre').to_dict(
     orient='dict')['id']
-actors = pd.read_csv("actors.csv", index_col='actor').to_dict(
+actors = pd.read_csv("data/actors.csv", index_col='actor').to_dict(
     orient='dict')['id']
-directors = pd.read_csv("directors.csv", index_col='director').to_dict(
+directors = pd.read_csv("data/directors.csv", index_col='director').to_dict(
     orient='dict')['id']
 
 # Creamos el diccionario de meses para la ruta get months
